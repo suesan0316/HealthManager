2dc184f1eef960720c79451b5dccd1ffcba7f5d9
Shunsuke Suzuki <suesan8942@gmail.com>
2017-10-11 00:14:22 +0900

    commit

--- HealthManager/HealthManager/View/DataChartView.xaml.cs
+++ HealthManager/HealthManager/View/DataChartView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using HealthManager.Common;
+using HealthManager.Extention;
 using HealthManager.Model.Service;
 using HealthManager.ViewModel;
 using Microcharts;
@@ -32,6 +33,8 @@ namespace HealthManager.View
 
             List<Entry> entries = null;
 
+            var dataList = new List<string>();
+
             switch (target)
             {
                 case BasicDataEnum.BodyWeight:
@@ -40,8 +43,12 @@ namespace HealthManager.View
                             Color = SKColor.Parse("#00CED1"),
                             Label = value.RegistedDate.ToString(),
                             ValueLabel = value.BodyWeight.ToString()
-                        })
+                        }
+                    )
                         .ToList();
+
+                    list.ForEach(data => dataList.Add(data.RegistedDate + "    " + data.BodyWeight + "Kg"));
+
                     break;
 
                 case BasicDataEnum.BodyFatPercentage:
@@ -52,12 +59,16 @@ namespace HealthManager.View
                             ValueLabel = value.BodyFatPercentage.ToString()
                         })
                         .ToList();
+
+                    list.ForEach(data => dataList.Add(data.RegistedDate + "    " + data.BodyFatPercentage + "%"));
+
                     break;
 
                     default: break;
             }
 
             DataChart.Chart = new LineChart { Entries = entries };
+            DataList.ItemsSource = dataList;
         }
     }
 }
