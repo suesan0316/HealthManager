a25488fdfd3133ad9e35f1fb5b8a0fc8545e5e24
Shunsuke Suzuki <suesan8942@gmail.com>
2018-03-10 12:01:38 +0900

    commit

--- HealthManager/HealthManager/Common/Other/ApplicationIinitializer.cs
+++ HealthManager/HealthManager/Common/Other/ApplicationIinitializer.cs
@@ -24,17 +24,18 @@ namespace HealthManager.Common.Other
                 try
                 {
                     db.DropTable<InitModel>();
-                    db.DropTable<BodyImageModel>();
-                    //db.DropTable<BasicDataModel>();
                     db.DropTable<LoadModel>();
                     db.DropTable<PartModel>();
                     db.DropTable<SubPartModel>();
                     db.DropTable<LoadUnitModel>();
                     db.DropTable<LocationModel>();
-                   db.DropTable<TrainingMasterModel>();
-                    db.DropTable<TrainingMenuModel>();
-                    db.DropTable<TrainingScheduleModel>();
-                    db.DropTable<TrainingResultModel>();
+
+                    //db.DropTable<BodyImageModel>();
+                    //db.DropTable<BasicDataModel>();
+                    //db.DropTable<TrainingMasterModel>();
+                    //db.DropTable<TrainingMenuModel>();
+                    //db.DropTable<TrainingScheduleModel>();
+                    //db.DropTable<TrainingResultModel>();
 
                 }
                 catch (NotNullConstraintViolationException e)
